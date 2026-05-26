75ca17e4bd8fb29bdf204b83174400aa48d7819a
KristinaHejlesen <kkhejlesen@gmail.com>
2026-05-26 17:26:41 +0200

    Tilføjet en masse styling på forsiden

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -9,6 +9,7 @@
  /*når der klikkes på menu ikonen, tilføjes classen aktiv og nav indholdet vises. */
 //  der er tilføjet en if statemate, hvis disse er til steder i DOM gør følgende
   if (burger && mobileMenu) {
+    // tilføjer evnetlistner der lytter efter klik på #menu, burgermenuen
     burger.addEventListener("click", () => {
       mobileMenu.classList.toggle("active");
       /*tilføjer class active til burgermenu ikonet, dette bruges til at lave animationen fra tre streger til et kryds */
@@ -25,7 +26,7 @@
   /*looper igennem alle knapper med class dropbtn og lytter efter click.  */
   btnNav.forEach((btn, i) => {
     btn.addEventListener("click", () => {
-      /*nextElementSibling returne det næste element, som i dette tilfælde er vores dropdownIndhold, der tilføjer jeg class show */
+      /*nextElementSibling returne det næste element, som i dette tilfælde er div med dropdown indholdet, der tilføjer jeg class show */
       const nextSibling = btn.nextElementSibling;
      
       if (nextSibling) {
@@ -37,8 +38,8 @@
     });
   });
   
-/*lukker dropdown når der klikkes udenfor boksen */
-/*kode lavet med hjælp fra https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_dropdown_navbar_click */
+/*lukker dropdown når der klikkes udenfor boksen, f.eks et andet sted på siden */
+/*kode lavet med efter følgende kilde: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_dropdown_navbar_click */
 
 const existingOnclick = window.onclick;
 window.onclick = function(luk) {
